19b90062357a12633efad21e31cca2415639e3b2
Alexandr Maxalov <sanya.maxalov@gmail.com>
2019-02-07 01:05:47 +0300

    v1: edit

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 function healthCheck(obj) {
 	if(obj.health < 15){
 		return "critical"
-	} else if (obj.health < 50 || obj.health < 15) {
+	} else if (obj.health < 50) {
 		return "wounded"
 	} else {
 		return "healthy"
